0541ea8129063d544df8bf5364624ab42fffc451
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-11-01 20:05:21 +0100

    commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
