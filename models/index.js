00362aef4970e40d6a98c17bb4909c151a45f28a
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-11-08 08:36:21 +0100

    commit

--- models/index.js
+++ models/index.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.contacts = require("./contact.models.js")(mongoose);
+db.contacts = require("../models/contacts.js")(mongoose);
 
 module.exports = db;
