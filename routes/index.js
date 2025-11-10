1550446bc3f2f82231931f95c60e77ec0fb637e6
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-11-10 09:31:07 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+// Home route
+router.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to the Henry's Contacts API!",
+    documentation: "/api-docs",
+    endpoints: {
+      "GET /contacts": "Get all contacts",
+      "GET /contacts/:id": "Get contact by ID",
+      "POST /contacts": "Create new contact",
+      "PUT /contacts/:id": "Update contact",
+        "DELETE /contacts/:id": "Delete contact",
+      "Swagger Docs" : "/api-docs",
+    },
+  });
+});
+
 router.use("/", require("./swagger"));
 router.use("/contacts", require("./contacts"));
 
